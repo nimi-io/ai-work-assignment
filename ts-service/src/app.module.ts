6b66bbdfb684c59d889d38e16b0095041d78b77c
nimi <ibukunoluwaodunsi@gmail.com>
2026-03-10 14:22:23 +0100

    Update app.module.ts

--- ts-service/src/app.module.ts
+++ ts-service/src/app.module.ts
@@ -1,5 +1,7 @@
+import path from 'path';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -15,6 +17,38 @@ import { FakeAuthGuard } from './auth/fake-auth.guard';
 @Module({
   imports: [
     ConfigModule.forRoot(configModuleOpts),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const db =
+          configService.get<
+            () => {
+              host: string;
+              port: number;
+              username: string;
+              password: string;
+              database: string;
+              ssl: boolean;
+              synchronize: boolean;
+              logging: boolean;
+            }
+          >('database')!();
+        return {
+          type: 'postgres' as const,
+          host: db.host,
+          port: db.port,
+          username: db.username,
+          password: db.password,
+          database: db.database,
+          ssl: db.ssl,
+          synchronize: db.synchronize,
+          logging: db.logging,
+          entities: [path.join(__dirname, '**/*.entity.{ts,js}')],
+          migrations: [path.join(__dirname, 'database/migrations/*.{ts,js}')],
+          autoLoadEntities: true,
+        };
+      },
+    }),
     AuthModule,
     CandidatesModule,
     DocumentsModule,
