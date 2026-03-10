44fbc5495990c2b32507d26ed0e60805eaac0d99
nimi <ibukunoluwaodunsi@gmail.com>
2026-03-10 14:21:46 +0100

    Update summarization.module.ts

--- ts-service/src/summarization/summarization.module.ts
+++ ts-service/src/summarization/summarization.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GeminiProvider } from './providers/gemini.provider';
+import { ProviderService } from './providers/index.provider.service';
 
-@Module({})
+@Module({
+  imports: [ConfigModule],
+  providers: [GeminiProvider, ProviderService],
+  exports: [ProviderService],
+})
 export class SummarizationModule {}
