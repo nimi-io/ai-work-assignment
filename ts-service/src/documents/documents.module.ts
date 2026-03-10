bcca7ca30cd1f6def84fe0991606406b409e6fe5
nimi <ibukunoluwaodunsi@gmail.com>
2026-03-10 14:22:15 +0100

    Update documents.module.ts

--- ts-service/src/documents/documents.module.ts
+++ ts-service/src/documents/documents.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DocumentsService } from './documents.service';
 import { DocumentsController } from './documents.controller';
+import { DocumentsRepository } from './documents.repository';
+import { CandidateDocument } from './entities/document.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CandidateDocument])],
   controllers: [DocumentsController],
-  providers: [DocumentsService],
+  providers: [DocumentsService, DocumentsRepository],
+  exports: [DocumentsRepository],
 })
 export class DocumentsModule {}
