b1eaf52fadfdcf4ee58da4e8fc0f46789cab5ae9
nimi <ibukunoluwaodunsi@gmail.com>
2026-03-10 23:50:19 +0100

    Update summaries.module.ts

--- ts-service/src/summaries/summaries.module.ts
+++ ts-service/src/summaries/summaries.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SummariesService } from './summaries.service';
 import { SummariesController } from './summaries.controller';
@@ -6,12 +6,14 @@ import { SummariesRepository } from './summaries.repository';
 import { CandidateSummary } from './entities/summary.entity';
 import { CandidatesModule } from '../candidates/candidates.module';
 import { SummarizationModule } from '../summarization/summarization.module';
+import { QueueModule } from 'src/shared/queue/queue.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([CandidateSummary]),
     CandidatesModule,
     SummarizationModule,
+    forwardRef(() => QueueModule),
   ],
   controllers: [SummariesController],
   providers: [SummariesService, SummariesRepository],
