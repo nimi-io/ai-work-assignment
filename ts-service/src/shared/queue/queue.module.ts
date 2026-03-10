48ecfa77303394e6e9605401642b343d0ce42760
nimi <ibukunoluwaodunsi@gmail.com>
2026-03-10 23:50:34 +0100

    Create queue.module.ts

---
+++ ts-service/src/shared/queue/queue.module.ts
@@ -0,0 +1,31 @@
+import { BullModule } from '@nestjs/bullmq';
+import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { QueueService } from './queue.service';
+import { QueueProcessor } from './queue.processor';
+import { SUMMARY_QUEUE } from '../constants/index.constant';
+import { SummariesModule } from 'src/summaries/summaries.module';
+import { CandidatesModule } from 'src/candidates/candidates.module';
+import { SummarizationModule } from 'src/summarization/summarization.module';
+
+@Module({
+  imports: [
+    forwardRef(() => SummariesModule),
+    CandidatesModule,
+    SummarizationModule,
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        connection: {
+          host: config.get<string>('REDIS_HOST', 'localhost'),
+          port: config.get<number>('REDIS_PORT', 6379),
+        },
+      }),
+    }),
+    BullModule.registerQueue({ name: SUMMARY_QUEUE }),
+  ],
+  providers: [QueueService, QueueProcessor],
+  exports: [QueueService],
+})
+export class QueueModule {}
