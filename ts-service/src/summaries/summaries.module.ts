03e0cf53fddc9d375747529813fae9b184aaae31
nimi <ibukunoluwaodunsi@gmail.com>
2026-03-10 14:21:55 +0100

    Update summaries.module.ts

--- ts-service/src/summaries/summaries.module.ts
+++ ts-service/src/summaries/summaries.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SummariesService } from './summaries.service';
 import { SummariesController } from './summaries.controller';
+import { SummariesRepository } from './summaries.repository';
+import { CandidateSummary } from './entities/summary.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CandidateSummary])],
   controllers: [SummariesController],
-  providers: [SummariesService],
+  providers: [SummariesService, SummariesRepository],
+  exports: [SummariesRepository],
 })
 export class SummariesModule {}
