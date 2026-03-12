e6770b076b148a0a2e16f6e40de54b4d1c426c2b
nimi <ibukunoluwaodunsi@gmail.com>
2026-03-12 13:51:25 +0100

    updates

--- ts-service/src/shared/decorators/public-request.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata, CustomDecorator } from '@nestjs/common';
-import { IS_PUBLIC_KEY } from 'src/shared/constants/index.constant';
-
-export const IsPublic = (): CustomDecorator => SetMetadata(IS_PUBLIC_KEY, true);
--- ts-service/src/workspaces/dto/create-workspace.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateWorkspaceDto {
-  @ApiProperty({ example: 'Acme Corp', description: 'Workspace display name' })
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-}
